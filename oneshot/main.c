455f5b146fa83ec1bf1ac365ca7165742426ef66
Mike Bell <mdb036@gmail.com>
2024-01-13 18:37:14 +0000

    UART

--- oneshot/main.c
+++ oneshot/main.c
@@ -1,5 +1,16 @@
 #include <stdint.h>
 
+void uart0_putc(int c) {
+    // send character to UART0
+    *((volatile uint32_t*)(0x40030000)) = c & 0xFF;
+}
+
+void uart0_puts(const char* s) {
+    while (*s) {
+        uart0_putc(*s++);
+    }
+}
+
 void main() {
     // func: SYS_RIO, output and output enable from peripheral
     *((volatile uint32_t*)(0x400d008c)) = 0x85;
@@ -11,5 +22,8 @@ void main() {
     *((volatile uint32_t*)(0x400e2004)) = (1 << 17);
 
     // toggle output
-    *((volatile uint32_t*)(0x400e1000)) = (1 << 17);    
+    *((volatile uint32_t*)(0x400e1000)) = (1 << 17);
+
+    const char* hello = "Hello, from RP1!\r\n";
+    uart0_puts(hello);
 }
--- oneshot/start.s
+++ oneshot/start.s
@@ -13,12 +13,31 @@ _entry:
     ldr r7, =0xf04f281f
     str r7, [r6]
 
+    /* Setup UART 0 */
+	ldr r2, =0x40030000   // UART0 SET BASE
+	movs r1, #0x1b
+	str r1, [r2, #0x24]   // Set divisor to 115200 baud
+	movs r1, #0x8
+	str r1, [r2, #0x28]
+	movs r1, #0x70
+	str r1, [r2, #0x2c]   // UART LCR_H. Enable FIFOs, 8 bits, 1 stop, no parity
+	mov r1, #0x101
+	str r1, [r2, #0x30]   // UART CR: Enable, transmit only
+
+	mov r1, #0x84           // Function 4 (UART0)
+    ldr r2, =0x400d0000
+	str r1, [r2, #0x74]   // GPIO 14
+
+	mov r1, #0x16           // Pads
+    ldr r2, =0x400f0000
+	str r1, [r2, #0x3c]   // GPIO 14
+
     /* Clear bss */
     ldr     r4, =__bss_start__
     ldr     r5, =__bss_end__
     cmp     r4, r5
     bge     1f
-    mov     r6, #0
+    movs    r6, #0
     mov     r7, r6
 
 _bss_loop:
